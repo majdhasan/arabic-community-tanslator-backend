a169f19cbb3ce839887a3801e64c38c4255d823c
Majd Hasan <majd.hasan@aperto.com>
2020-11-16 22:12:42 +0100

    backend init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a09628ab1a45586afe489db7450fc047'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec84b8ae-f850-4c84-8f91-bd4e4fb433ed'
+};
